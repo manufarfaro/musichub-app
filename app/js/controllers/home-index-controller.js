842e2c568231f1f923460e0ecd2cff342844f7b6
manufarfaro <manufarfaro@gmail.com>
2015-07-23 16:23:03 -0300

    #28: Done.

--- app/js/controllers/home-index-controller.js
+++ app/js/controllers/home-index-controller.js
@@ -4,9 +4,9 @@
   angular.module('musicHub')
     .controller('HomeIndexController', homeIndexController);
 
-  homeIndexController.$inject = ['$sce', '$http', 'VideoService','PhotoService'];
+  homeIndexController.$inject = ['$http', 'VideoService','PhotoService'];
 
-  function homeIndexController($sce, $http, VideoService, PhotoService) {
+  function homeIndexController($http, VideoService, PhotoService) {
     var vm = this;
     vm.videos = VideoService.random({limit: 5});
     vm.photos = PhotoService.random({limit: 10});
